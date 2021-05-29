664f6265c962220e876db584218a5a71b39fbc81
Halleluwah <datsuzoku@tutanota.com>
2021-05-29 15:40:17 +0200

    Added support for EMODB

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,21 +1,74 @@
 import os
 import numpy as np
 import torch
+from torch.utils import data
 import torchaudio
 from torch.utils.data import Dataset
 
+
+class EMODB(Dataset):
+    """EMODB: German emotional speech database
+
+    Args:
+        root_dir (string): Root directory of the wav files of the
+            dataset.
+        transforms (object, optional): Callable class with all
+            transforms to be performed. Default: None
+        training (boolean, optional): True for training set, False
+            for validation set. Default: True
+    """
+    def __init__(self, root_dir, transforms=None, training=True):
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.emotions = {k: i for i, k in enumerate([
+            'W',  # Ärger (Wut) - anger
+            'L',  # Langeweile  - boredom
+            'E',  # Ekel        - disgust
+            'A',  # Angst       - anxiety/fear
+            'F',  # Freude      - happiness
+            'T',  # Trauer      - sadness
+            'N',  #             - neutral version
+        ])}
+        self.filenames = os.listdir(self.root_dir)
+        delimiter = int(len(self.filenames) * .8)  # delimiter between training and validation
+        if training:
+            self.filenames = self.filenames[:delimiter]
+        else:
+            self.filenames = self.filenames[delimiter:]
+    
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        # load wave file
+        path = os.path.join(self.root_dir, self.filenames[idx])
+        data, _ = torchaudio.load(path)
+
+        # to mono
+        data = torch.mean(data, 0)
+
+        # apply transforms, if needed
+        if self.transforms is not None:
+            data = self.transforms(data)
+
+        data = torch.unsqueeze(data, 0)
+
+        # parse filename to get emotion
+        emotion = self.emotions[path[-6]]  # _x_.wav -> x is the emotion code
+
+        return (data, emotion)
 
 class EMOVO(Dataset):
     """EMOVO: Italian emotional speech database
 
     Args:
         root_dir (string): Root directory of the dataset.
-        transforms (object, optional): Callable class with all transforms to be
-            performed. Default: None
+        transforms (object, optional): Callable class with all
+            transforms to be performed. Default: None
         training (boolean, optional): True for training set, False
             for validation set. Default: True
     """
-    def __init__(self, root_dir, transforms, training=True):
+    def __init__(self, root_dir, transforms=None, training=True):
         self.root_dir = root_dir
         self.transforms = transforms
         self.emotions = [
@@ -52,6 +105,7 @@ class EMOVO(Dataset):
         emotion_idx = idx % self.per_actor // len(self.sentence_types)
         sentence_type_idx = idx % self.per_actor % len(self.sentence_types)
 
+        # load wave file
         path = os.path.join(self.root_dir, self.actors[actor_idx],
             '{}-{}-{}.wav'.format(
                 self.emotions[emotion_idx][:3],
@@ -60,8 +114,11 @@ class EMOVO(Dataset):
             )
         )
         data, _ = torchaudio.load(path)
-        data = torch.mean(data, 0)  # to mono
 
+        # to mono
+        data = torch.mean(data, 0)
+
+        # apply transforms, if needed
         if self.transforms is not None:
             data = self.transforms(data)
 
